d1274d0cc74c76bf88f8d2c764738f656e7ec863
regfs <regfs@PCK>
2024-03-04 15:26:22 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int rebr, versh;
+    cout << "Введите количество рёбер: \n";
+    cin >> rebr;
+    cout << "Введите количество вершин: \n";
+    cin >> versh;
+    std::cout << "Hello World!\n";
+}
