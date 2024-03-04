1e0410abbb8121b0e7da91d904bfdaf0cf1a62bd
zolotov0 <109308004+zolotov0@users.noreply.github.com>
2024-03-04 20:04:12 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,25 @@
 ﻿#include <iostream>
+
 using namespace std;
+
 int main()
 {
-    int rebr, versh;
-    cout << "Введите количество рёбер: \n";
-    cin >> rebr;
-    cout << "Введите количество вершин: \n";
-    cin >> versh;
-    std::cout << "Hello Worladawdad!\n";
+    int a, b;
+    srand(time(NULL));
+    cout << "Введите кол-во вершин";
+    cin >> a;
+
+    cout << "Введите кол-во рёбер";
+    cin >> b;
+
+    int** matrix = new int* [a];
+    for (int i = 0; i < a; i++) {
+        matrix[i] = new int[a];
+        for (int j = 0; j < a; j++) {
+
+            matrix[i][j] = rand() % 10 - 5;
+        }
+    }
+
+
 }
